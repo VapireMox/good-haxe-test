d026b43170d737de304b5bacf7abd30e93d3f590
GeXie19 <3449326235@qq.com>
2025-02-18 05:08:23 +0800

    Create Main.hx

---
+++ thread/source/Main.hx
@@ -0,0 +1,13 @@
+package;
+
+class Main {
+  public static function main():Void {
+    sys.thread.Thread.create(() -> {
+      while (true) {
+        trace("other thread");
+        Sys.sleep(1);
+      }
+    });
+    Sys.sleep(3);
+  }
+}
