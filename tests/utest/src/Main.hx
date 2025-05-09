340eda60b463dd919cf176cf954282af3d85ada2
VapireMox <3449326235@qq.com>
2025-05-09 18:01:22 +0800

    test1

---
+++ tests/utest/src/Main.hx
@@ -0,0 +1,14 @@
+package;
+
+import utest.UTest;
+import tests.*;
+
+class Main {
+	public static function main() {
+		UTest.run([new TestA()], onComplete);
+	}
+
+	static function onComplete() {
+		trace("Test Finish");
+	}
+}
---
+++ tests/utest/src/tests/TestA.hx
@@ -0,0 +1,14 @@
+package tests;
+
+import utest.Assert;
+import utest.Test;
+
+class TestA extends Test {
+	public function new() {
+	
+	}
+
+	public function firstTest() {
+		Assert.equals(1 + 1, 2);
+	}
+}
